b0e550af053aaf7f804e6c4cc1cf9fe041572d5c
CrabRus <goncarovruslan322@gmail.com>
2024-04-10 17:39:59 +0300

    0.0.6

--- game.py
+++ game.py
@@ -1,28 +1,35 @@
 import time
 import random
 
-class Node:
+class Room:
     def __init__(self, name, number, edges=[], availability = False):
         self.name = name
         self.number = number
         self.edges = edges
         self.availability = False
 
-r_Hall = Node("Правый холл",1,[2,5], False)
-scene = Node("Сцена",2,[1,3], True)
-l_Hall = Node("Левый холл",3,[2,6], False)
-office = Node("Офис охранника",4, [], False)
-r_pocket = Node("Правый кармашек", 5, [1], False)
-l_pocket = Node("Левый кармашек", 6, [3], False)
+class Office(Room):
+    def __init__(self,name, number, edges=[], availability = False, r_door = False, l_door = False):
+        self.r_door = r_door
+        self.l_door = l_door
+
+
+l_Hall = Room("Левый холл",1,[2,5], False)
+scene = Room("Сцена",2,[1,3], True)
+r_Hall = Room("Правый холл",3,[2,6], False)
+office = Office("Офис охранника",4, [], False, True, False)
+l_pocket = Room("Левый кармашек", 5, [1], False)
+r_pocket = Room("Правый кармашек", 6, [3], False)
 
 rooms = [r_Hall, scene, l_Hall, r_pocket, l_pocket]
 
 class Animatronic:
-    def __init__(self, name="", number = 1, pre_location = 0, real_location = 2):
+    def __init__(self, name="", number = 1, pre_location = 0, real_location = 2, trajectory = []):
         self.name = name
         self.number = number
         self.pre_location = pre_location
         self.real_location = real_location
+        self.trajectory = trajectory
     def move(self):
         for i in rooms:
             if i.number == self.real_location:
@@ -35,14 +42,56 @@ class Animatronic:
                 print(f"{self.name} перешел в: {next_location}")
                 break
 
-freddy = Animatronic("Отбитый на голову Фредди",1, 0, 2)
+
+
+    def move_trajectory(self):
+        real_trajectory = random.choice(self.trajectory)
+        for i in rooms:
+            if i.number == self.real_location:
+                for y in real_trajectory:
+                    self.real_location = y
+                    for z in rooms:
+                        if z.number == self.real_location:
+                            next_location = z.name
+                    print(f"{self.name} перешел в: {next_location}")
+                    self.attack_office()
+                    time.sleep(3)
+                break
+
+    def attack_office(self):
+        if self.real_location == 5:
+            time.sleep(random.choice([7, 8, 9, 10]))
+            final_choice = random.choice([True, False])
+            if final_choice == True:
+                if office.l_door == False:
+                    print("Отбитый на голову Фредди вошел в офис офранника с левой двери")
+                else:
+                    self.real_location = 2
+                    print("Отбитый на голову Фредди вернулся на сцену")
+
+        if self.real_location == 6:
+            time.sleep(random.choice([10]))
+            final_choice = random.choice([True, False])
+            if final_choice == True:
+                if office.r_door == False:
+                    print("Отбитый на голову Фредди вошел в офис офранника с правой двери")
+                else:
+                    self.real_location = 2
+                    print("Отбитый на голову Фредди вернулся на сцену")
+
+
+
+freddy = Animatronic("Отбитый на голову Фредди",1, 0, 2, [[1,5],[3,6]])
 
 animatronics = [freddy]
-for i in range(10):
-    freddy.move()
-    time.sleep(1)
+# for i in range(10):
+#     freddy.move()
+#     time.sleep(1)
 
-#def start_game(animatronics, rooms):
+# def start_game(animatronics, rooms):
+#
 
 
 
+
+freddy.move_trajectory()
